e0de83c1104bfd51f00a8722e1fb4cc9094ef680
samanthadotdesign <samleeml@gmail.com>
2021-09-09 22:42:21 +0800

    added custom renderer for markdown with bugs

--- components/Posts/PostDetail/PostContent.js
+++ components/Posts/PostDetail/PostContent.js
@@ -1,4 +1,9 @@
+import React from 'react';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { atomDark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 import ReactMarkdown from 'react-markdown';
+import Image from 'next/image';
 import PostHeader from './PostHeader';
 
 export default function PostContent(props) {
@@ -8,13 +13,54 @@ export default function PostContent(props) {
 
   const imagePath = `/images/work/${slug}/${image}`;
 
+  // Create custom renderers to optimize images with next/image instead of standard markdown
+  const customRenderers = {
+    // All markdown (not titles) translated to HTML is treated as paragraph
+    // Mitigates how image is rendered as paragraph
+    p(paragraph) {
+      const { node } = paragraph;
+      if (node.children[0].tagName === 'img') {
+        const img = node.children[0];
+
+        return (
+          <div>
+            <Image
+              src={`/images/work/${slug}/${img.properties.src}`}
+              alt={img.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+    code(code) {
+      const { className, children } = code;
+      // className is something like language-js => We need the "js" part here
+      const language = className.split('-')[1];
+
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
+    },
+  };
+
   return (
     <article>
       <PostHeader
         title={title}
         image={imagePath}
       />
-      <ReactMarkdown>{content}</ReactMarkdown>
+      <ReactMarkdown
+        components={customRenderers}
+      >
+        {content}
+      </ReactMarkdown>
     </article>
   );
 }
--- posts/designer-starter-pack.md
+++ posts/designer-starter-pack.md
@@ -6,4 +6,25 @@ isFeatured: true
 
 # Designer Starter Pack
 
-React Development
+Designer Starter Pack is a curated list of Figma skills.
+
+This list exists to better organize the list of skills I've come to know, like, and/or find interesting while working in product design.
+
+## Tech stack
+
+The first version was created using EJS and NodeJS.
+The second version was recreated using ReactJS.
+
+![Create routes via your file + folder structure](nextjs-file-based-routing.png)
+
+## Learnings
+
+```js
+const basics = "Testing";
+
+function printBasics() {
+  console.log(basics);
+}
+
+printBasics();
+```
