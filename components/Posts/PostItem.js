b7d47d5f67c7f9a799eba58066577771e2e695d9
samanthadotdesign <samleeml@gmail.com>
2021-09-14 06:56:56 +0800

    added draggable functionality with setTimeout to delay clicking into post right away

--- components/Posts/PostItem.js
+++ components/Posts/PostItem.js
@@ -1,4 +1,6 @@
-import Link from 'next/link';
+import { useRouter } from 'next/router'
+
+
 import Image from 'next/image';
 import Draggable from 'react-draggable';
 import { useState, useEffect } from 'react'
@@ -6,9 +8,9 @@ import { useState, useEffect } from 'react'
 export default function PostGrid(props) {
   const { title, image, slug } = props.post;
 
-  const [position, setPosition] = useState( 
-    {x: 0, 
-    y: 0} )
+  const [position, setPosition] = useState( {x: 0, y: 0} )
+
+  const [isDragging, setIsDragging] = useState(false)
 
   // window height and width are rendered in the frontend
   
@@ -25,28 +27,55 @@ export default function PostGrid(props) {
     setPosition( { x: data.x, y: data.y})
   }
 
+  const handleDrag = (e, data) => {
+    setIsDragging(true)
+    trackPos(data)
+    //console.log("CHECKING ISDRAGGING ON DRAG", isDragging)
+  }
+
+  const onDragStop = ()=>{
+
+    console.log("CHECHING VARIABLE BEFORE STOPING", isDragging)
+    setTimeout(() => {
+      setIsDragging(false)
+      setTimeout(()=>{
+        console.log("CHECHING VARIABLE AFTER STOPING", isDragging)
+      }, 10)
+      
+    }, 10);
+  }
+
+  const router = useRouter()
+
+  const goToLink = (/* event */) =>{
+    //event.preventDefault()
+    console.log("CHECK ISDRAGGING VARIABLER", isDragging)
+    if(!isDragging) {
+      console.log("GOING TO LINK", isDragging)
+      router.push(linkPath)}
+  } 
+
   return (
     <Draggable
       position={position}
-      onDrag={(e, data) => trackPos(data)}>
-      <div>
-        <Link href={linkPath}>
-          <a>
-            <div>
-              <Image
-                src={imagePath}
-                alt={title}
-                width={200}
-                height={100}
+      onDrag={handleDrag}
+      onStop={onDragStop}
+      >
+        <div onClick={goToLink}>
+          <div>
+            <Image
+              src={imagePath}
+              alt={title}
+              width={200}
+              height={100}
+              className="userSelectNone"
               />
-            </div>
-            <div>
-              <h3>{title}</h3>
-              <p>Interaction Design</p>
-            </div>
-          </a>
-        </Link>
-      </div>
+          </div>
+          <div>
+            <h3>{title}</h3>
+            <p>Interaction Design</p>
+          </div>
+        </div>
     </Draggable>
   );
 }
