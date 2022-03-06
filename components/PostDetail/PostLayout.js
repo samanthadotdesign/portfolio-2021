efb354befab75e39d007b5da684c2014fdcad6d7
samanthadotdesign <samleeml@gmail.com>
2022-03-06 16:58:38 +0800

    edited case study content

--- components/PostDetail/PostLayout.js
+++ components/PostDetail/PostLayout.js
@@ -1,13 +1,11 @@
-import React from 'react';
+import React from "react";
 
-export default function PostLayout({children}) {
-	return (
-		<div className="container-fluid d-flex flex-column margin-top-36">
-			<div className="row">
-				<div className="col-md-6 px-0">
-					{children}
-				</div>
-			</div>
-		</div>
-	);
+export default function PostLayout({ children }) {
+  return (
+    <div className="container-fluid d-flex flex-column margin-top-36">
+      <div className="row">
+        <div className="col-md-5 px-0">{children}</div>
+      </div>
+    </div>
+  );
 }
