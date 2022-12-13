856fb5c31125e56fc60b895ff7193a6053bc7509
samanthadotdesign <samleeml@gmail.com>
2022-12-13 13:17:35 +0800

    added background to case study

---
+++ components/PostDetail/ProcessVisual.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/**
+ * Given an array of items i.e. Step 1, Step 2,
+ * @param {*} props – array of titles
+ * @returns – dynamic circles
+ */
+export default function ProcessVisual(props) {
+  return <div></div>;
+}
