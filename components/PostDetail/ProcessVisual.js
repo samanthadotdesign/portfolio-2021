448c627f6ea1007b1ae7da351719bce66e1f6b95
samanthadotdesign <samleeml@gmail.com>
2022-12-22 14:46:03 +0800

    set up process circle component for blog post

--- components/PostDetail/ProcessVisual.js
+++ components/PostDetail/ProcessVisual.js
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useContext, useState, useEffect } from "react";
+import { GlobalContext } from "../../store";
+import { ArrowSVG } from "../../public/images/work/protecting-meta/process-arrow.svg";
 
 /**
  * Given an array of items i.e. Step 1, Step 2,
- * @param {*} props – array of titles
+ * @param props – array of titles
  * @returns – dynamic circles
  */
 export default function ProcessVisual(props) {
-  return <div></div>;
+  const { headings } = props;
+  const { windowStoreState } = useContext(GlobalContext);
+  const { window } = windowStoreState;
+  const [isLargeDevice, setIsLargeDevice] = useState(false);
+
+  useEffect(() => {
+    if (window.width > 768) {
+      setIsLargeDevice(true);
+    }
+  }, []);
+
+  return (
+    <>
+      {headings.map((item) => {
+        return (
+          <div className="process-circle">
+            <p>{item}</p>
+          </div>
+        );
+      })}
+    </>
+  );
 }
