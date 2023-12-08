12afd5692bbdf1fd4259237f9fb15e1d1c4d0fae
samanthadotdesign <samleeml@gmail.com>
2023-12-08 22:18:41 +0800

    added hover interaction

--- components/Posts/NeatPost.js
+++ components/Posts/NeatPost.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React, { useRef, useState, useMemo } from "react";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import LoopingVideo from "../PostDetail/LoopingVideo";
@@ -6,7 +6,7 @@ import LoopingVideo from "../PostDetail/LoopingVideo";
 export default function NeatPost(props) {
   const { post } = props;
   const { slug, frontMatter, mdxSource } = post;
-  const [isHover, setIsHover] = useState(false);
+  const [isHovered, setHovered] = useState(false);
   const { title, media, mask, color } = frontMatter;
   const postItemRef = useRef(null);
   const mediaPath = `/images/work/${slug}/${media}`;
@@ -16,19 +16,20 @@ export default function NeatPost(props) {
     router.push(linkPath);
   };
 
+  const handleMouseEnter = useMemo(() => () => setHovered(true), []);
+  const handleMouseLeave = useMemo(() => () => setHovered(false), []);
+
   return (
     <div
-      className={`d-flex flex-column justify-content-center hovered-card`}
-      // ${
-      //   isHover && "hovered-card"
-      // }`}
+      className={`d-flex flex-column justify-content-center
+      ${isHovered && "hovered-card"}`}
       onTouchStart={goToLink}
       onClick={goToLink}
-      // onMouseEnter={setIsHover(true)}
-      // onMouseLeave={setIsHover(false)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <div className="neat-view-img"></div>
-      <div className="d-flex flex-row">
+      <div className="d-flex flex-row pb-2">
         <div
           style={{
             backgroundColor: `${color}`,
@@ -36,9 +37,11 @@ export default function NeatPost(props) {
             WebkitMaskImage: `url("/images/site/${mask}")`,
           }}
           className="masked neat-view-icon"
-          mediaPath={mediaPath}
+          mediapath={mediaPath}
         ></div>
-        <p className="m-0 align-self-center">{title}</p>
+        <p className={`m-0 align-self-center ${isHovered && "hovered-text"}`}>
+          {title}
+        </p>
       </div>
     </div>
   );
