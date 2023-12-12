9025bb4bacb81f8bb080160749e5e6dc62cdcd26
samanthadotdesign <samleeml@gmail.com>
2023-12-12 17:30:28 +0800

    fixed links on neat view grid

--- components/Posts/NeatPost.js
+++ components/Posts/NeatPost.js
@@ -16,33 +16,24 @@ export default function NeatPost(props) {
   return (
     <NextLink
       href={linkPath}
-      className="neat-view-container d-flex flex-column justify-content-center"
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
-      {/* {isHovered ? (
-        <video
-          src={mediaPath}
-          className="neat-view-media"
-          autoPlay={isHovered}
-          muted
-          loop
-        />
-      ) : ( */}
-      <img src={coverImagePath} className="neat-view-media" />
-      {/* )} */}
-      <div className="neat-view-title d-flex flex-row">
-        <div
-          style={{
-            backgroundColor: `${color}`,
-            maskImage: `url("/images/site/${mask}")`,
-            WebkitMaskImage: `url("/images/site/${mask}")`,
-          }}
-          className="masked neat-view-icon"
-        ></div>
-        <p className={`m-0 align-self-center ${isHovered && "hovered-text"}`}>
-          {title}
-        </p>
+      <div className="neat-view-container d-flex flex-column justify-content-center">
+        <img src={coverImagePath} className="neat-view-media" />
+        <div className="neat-view-title d-flex flex-row">
+          <div
+            style={{
+              backgroundColor: `${color}`,
+              maskImage: `url("/images/site/${mask}")`,
+              WebkitMaskImage: `url("/images/site/${mask}")`,
+            }}
+            className="masked neat-view-icon"
+          ></div>
+          <p className={`m-0 align-self-center ${isHovered && "hovered-text"}`}>
+            {title}
+          </p>
+        </div>
       </div>
     </NextLink>
   );
